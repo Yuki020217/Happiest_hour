bf85a6d939dc315512180352afb70a0bd2d49fc8
Yuki020217 <65502008+Yuki020217@users.noreply.github.com>
2025-04-06 22:03:12 +1000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from mpi4py import MPI
-from data_loader import process_ndjson_file
-
-def merge_results(result, dict):
-  for key, value in dict.items():
-    if key in result:
-      result[key] += value
-    else:
-      result[key] = value
